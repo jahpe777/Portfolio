531e326037e4d62a9e5db29b86b2db8eed1a5b0f
James Higgs <james.y.higgs@gmail.com>
2019-07-18 15:52:13 -0700

    fixed repo

---
+++ assets/js/portfolio.js
@@ -0,0 +1,59 @@
+function james() {
+    $(".name").click(function(event) {
+        event.preventDefault();
+        $(".jamesModal").css("display","flex");
+    });
+    $(".close").click(function(event) {
+        event.preventDefault();
+        $(".jamesModal").css("display","none");
+    });
+}
+
+function about() {
+    $(".about").click(function(event) {
+        event.preventDefault();
+        $(".aboutModal").css("display","flex");
+    });
+    $(".close").click(function(event) {
+        event.preventDefault();
+        $(".aboutModal").css("display","none");
+    });
+}
+
+function project() {
+    $(".project").click(function(event) {
+        location.href = 'https://jahpe777.github.io/Quiz_App/';
+    });
+}
+
+function code() {
+    $(".code").click(function(event) {
+        location.href = 'https://github.com/jahpe777/Quiz_App.git';
+    });
+}
+
+function email() {
+    $("#email").click(function(event) {
+        location.href = 'mailto:james@jamesyhiggs.com';
+    });
+}
+
+function linkedin() {
+    $("#linkedin").click(function(event) {
+        location.href = 'https://www.linkedin.com/in/jameshiggs/';
+    });
+}
+
+function github() {
+    $("#github").click(function(event) {
+        location.href = 'https://github.com/jahpe777';
+    });
+}
+
+james();
+about();
+project();
+code();
+email();
+linkedin();
+github();
